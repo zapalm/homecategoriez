eb29318351bc9910a7b9083b2131aba47a2e1cb0
zapalm <zapalm@ya.ru>
2015-02-22 18:08:29 +1100

    // release of v1.1.0

---
+++ homecategoriez.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Home Categories Block: module for PrestaShop 1.3-1.4
+ *
+ * @author zapalm <zapalm@ya.ru>
+ * @copyright (c) 2012-2015, zapalm
+ * @link http://prestashop.modulez.ru/en/frontend-features/31-block-of-categories-on-the-homepage.html The module's homepage
+ * @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ */
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class HomeCategoriez extends Module
+{
+	private $conf_default = array(
+		'HOMECATEGORIEZ_CATALOG' => 1,
+		'HOMECATEGORIEZ_COLS' => 4,
+		'HOMECATEGORIEZ_WIDTH_ADJUST' => 538,	// ширина для блока из 4 колонок для стандартной темы Prestashop
+	);
+
+	public function __construct()
+	{
+		$this->name = 'homecategoriez';
+		$this->tab = version_compare(_PS_VERSION_, '1.4', '>=') ? 'front_office_features' : 'Tools';
+		$this->version = '1.1.0';
+		$this->author = 'zapalm';
+		$this->need_instance = 0;
+		$this->ps_versions_compliancy = array('min' => '1.3.0.0', 'max' => '1.4.12.0');
+		$this->bootstrap = false;
+
+		parent::__construct();
+
+		$this->displayName = $this->l('Categories on the homepage');
+		$this->description = $this->l('Displays categories in the middle of your homepage');
+	}
+
+	public function install()
+	{
+		foreach ($this->conf_default as $c => $v)
+			Configuration::updateValue($c, $v);
+
+		return parent::install() && $this->registerHook('home');
+	}
+
+	public function uninstall()
+	{
+		foreach ($this->conf_default as $c => $v)
+			Configuration::deleteByName($c);
+
+		return parent::uninstall();
+	}
+
+	public function getContent()
+	{
+		global $cookie;
+
+		$output = '<h2>'.$this->displayName.'</h2>';
+		
+		if (Tools::isSubmit('submit_save'))
+		{
+			$res = 1;
+			foreach ($this->conf_default as $v => $v)
+				$res &= Configuration::updateValue($v, intval(Tools::getValue($v)));
+
+			$output .= $res ? $this->displayConfirmation($this->l('Settings updated')) : $this->displayError($this->l('Some setting not updated'));
+		}
+
+		$conf = Configuration::getMultiple(array_keys($this->conf_default));
+		$categories = Category::getHomeCategories($cookie->id_lang, false);
+		$root_cat = Category::getRootCategory($cookie->id_lang);
+		$output .= '
+			<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+				<fieldset>
+					<legend><img src="'._PS_ADMIN_IMG_.'cog.gif" />'.$this->l('Settings').'</legend>
+					<label>'.$this->l('Root category of children categories to display').'</label>
+					<div class="margin-form">
+						<select name="HOMECATEGORIEZ_CATALOG">
+							<option value="'.$root_cat->id.'"'.($conf['HOMECATEGORIEZ_CATALOG'] == $root_cat->id ? ' selected="selected"' : '').'>'.$root_cat->name.'</option>';
+							foreach ($categories as $v)
+								$output .= '<option value="'.$v['id_category'].'"'.($conf['HOMECATEGORIEZ_CATALOG'] == $v['id_category'] ? ' selected="selected"' : '').'>'.$v['name'].'</option>';
+							$output .= '
+						</select>
+						<p class="clear">'.$this->l('Choose a root category (default : Home category).').'</p>
+					</div>
+					<label>'.$this->l('Number of columns to display').'</label>
+					<div class="margin-form">
+						<input type="text" size="1" name="HOMECATEGORIEZ_COLS" value="'.($conf['HOMECATEGORIEZ_COLS'] ? $conf['HOMECATEGORIEZ_COLS'] : '4').'" />
+						<p class="clear">'.$this->l('A number of columns to display on homepage (default: 4).').'</p>
+					</div>
+					<label>'.$this->l('Width adjust for the block of categories').'</label>
+					<div class="margin-form">
+						<input type="text" size="3" name="HOMECATEGORIEZ_WIDTH_ADJUST" value="'.($conf['HOMECATEGORIEZ_WIDTH_ADJUST'] ? $conf['HOMECATEGORIEZ_WIDTH_ADJUST'] : '0').'" /> px.
+						<p class="clear">'.$this->l('Input a number of pixels to adjust width of the block of categories.').'</p>
+					</div>
+					<center><input type="submit" name="submit_save" value="'.$this->l('Save').'" class="button" /></center>
+				</fieldset>
+			</form>
+			<br class="clear">
+		';
+
+		return $output;
+	}
+
+	public function hookHome($params)
+	{
+		global $smarty, $link;
+
+		$conf = Configuration::getMultiple(array_keys($this->conf_default));
+		$categories = Category::getChildren((int)$conf['HOMECATEGORIEZ_CATALOG'], (int)$params['cookie']->id_lang, true);
+
+		$block_width = (int)$conf['HOMECATEGORIEZ_WIDTH_ADJUST'];
+		$nb_items_per_line = (int)$conf['HOMECATEGORIEZ_COLS'];
+		$block_width_adjust = ceil($nb_items_per_line * 2) + 2;
+		$block_content_width = $block_width - $block_width_adjust;
+		$block_li_width = ceil($block_content_width / $nb_items_per_line);
+		$pic_size = 'home';
+
+		$smarty->assign(array(
+			'block_width' => $block_width,
+			'nb_items_per_line' => $nb_items_per_line,
+			'block_li_width' => $block_li_width,
+			'categories' => $categories,
+			'link' => $link,
+			'size_str' => $pic_size,
+			'pic_size' => Image::getSize($pic_size),
+		));
+
+		return $this->display(__FILE__, 'homecategoriez.tpl');
+	}
+}
---
+++ ru.php
@@ -0,0 +1,18 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_aa752afe21b351109f94ab2f9818f3b3'] = 'Категории на главной';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_cf51da77895b3efa8222bcbbc21a2ca3'] = 'Отображает категории на главной странице сайта';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_c888438d14855d7d96a2724ee9c306bd'] = 'Настройки сохранены';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_1e103a622ed29a413eadbb2839927386'] = 'Настройки сохранились не успешно';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_f4f70727dc34561dfde1a3c529b6205c'] = 'Настройки';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_4684166e0eb15d0d29556645805c2f58'] = 'Корневая категория для отображения дочерних категорий';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_2a2432ab8527cf815c1406178f74c9f9'] = 'Выберете корневую категорию (по-умолчанию: Домашняя)';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_10c48695e2bbbaf818d20d0cc9e1b6a1'] = 'Количество отображаемых колонок';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_fbe71263596f7f66fb0f888890b0a5f9'] = 'Число колонок для отображения на главной (по-умолчанию: 4)';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_1089ea6e4a508669165469b71b7298b6'] = 'Подгонка ширины блока категорий';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_97138b6ee81e6acd49ff0d043141ca25'] = 'Введите число пикселей для подгонки ширины блока категорий';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_c9cc8cce247e49bae79f15173ce97354'] = 'Сохранить';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_af1b98adf7f686b84cd0b443e022b7a0'] = 'Категории';
+$_MODULE['<{homecategoriez}prestashop>homecategoriez_87bdf2b1731f2c475caf4afc3eae14d0'] = 'Нет категорий';
